386c1341c26497d86be349e262d278251b99129c
Pablo Weisbek <pablo.weisbek@gmail.com>
2020-01-22 09:45:28 -0300

    basic local storage implemented

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
+
 # ajs-todo
-A simple ToDo app built with AngularJS
+
+
+```
+A simple ToDo web-app built with AngularJS.
+
+Add, delete or update your entries. Data is available via localStorage.
+
+Project styled using Sass and VSCode's Live Sass Compiler
+
+```
--- js/controllers/TodoController.js
+++ js/controllers/TodoController.js
@@ -1,17 +1,36 @@
-app.controller("TodoController", function ($scope) {
-  $scope.todos = [];
+app.controller("TodoController", function ($scope, $window) {
+  $scope.todos = []
+
+  if ($window.localStorage.length >= 1) {
+    console.log(angular.fromJson($window.localStorage.getItem('itemsList')))
+  } else { console.log('No localStorage found')}
 
   $scope.addItem = function () {
-    $scope.todos.push({
-      text: $scope.inputText,
-      done: false
-    });
-    $scope.inputText = "";
+    // prevent empty entries from being accepted
+    if (!$scope.inputText == '') {
+      //if user typed something , proceed
+      $scope.todos.push({
+        text: $scope.inputText,
+        done: false
+      });
+      $window.localStorage.setItem('itemsList', angular.toJson($scope.todos))
+      $scope.inputText = "";
+    }
   };
 
   $scope.remaining = function () {
     return $scope.todos.length;
   };
+
+  $scope.completed = function() {
+    let count = 0;
+    angular.forEach($scope.todos, function(todo) {
+      if (todo.done) {
+        count++
+      }
+    })
+    return count;
+  }
 
   $scope.clearCompleted = function () {
     var oldTodos = $scope.todos;
