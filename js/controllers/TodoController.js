868f19845e5c9aae606c37a310f5b93fef443759
pablowbk <pablo.weisbek@gmail.com>
2020-01-28 06:43:46 -0300

    fix initial load bug

--- js/controllers/TodoController.js
+++ js/controllers/TodoController.js
@@ -1,16 +1,18 @@
 app.controller("TodoController", function ($scope, $window) {
-  $scope.todos = []
+  $scope.todos = [];
+  $scope.inputText = "";
 
   if ($window.localStorage['itemsList']) {
     $scope.todos = angular.fromJson($window.localStorage.getItem('itemsList'));
-  } else { 
-      // $scope.todos = []; 
-      console.log('No localStorage found')
-    }
+    console.log('Fetched Items from localStorage', $scope.todos)
+  } else {
+    // $scope.todos = []; 
+    console.log('No localStorage found');
+  }
 
   $scope.addItem = function () {
     // prevent empty entries from being accepted
-    if (!$scope.inputText == '') {
+    if ($scope.inputText.length > 0) {
       //if user typed something , proceed
       $scope.todos.push({
         text: $scope.inputText,
@@ -26,9 +28,9 @@ app.controller("TodoController", function ($scope, $window) {
     return $scope.todos.length;
   };
 
-  $scope.completed = function() {
+  $scope.completed = function () {
     let count = 0;
-    angular.forEach($scope.todos, function(todo) {
+    angular.forEach($scope.todos, function (todo) {
       if (todo.done) {
         count++
       }
@@ -36,37 +38,51 @@ app.controller("TodoController", function ($scope, $window) {
     return count;
   }
 
-  $scope.toggleDone = function() {
+  $scope.toggleDone = function () {
     this.todo.done = !this.todo.done;
     $window.localStorage.setItem('itemsList', angular.toJson($scope.todos))
   }
 
   $scope.clearCompleted = function () {
-    var oldTodos = $scope.todos;
-    $scope.todos = [];
-    angular.forEach(oldTodos, function (todo) {
-      !todo.done && $scope.todos.push(todo);
-    });
-    $window.localStorage.setItem('itemsList', angular.toJson($scope.todos))
+    if ($scope.todos.length > 0) {
+      var oldTodos = $scope.todos;
+      $scope.todos = [];
+      angular.forEach(oldTodos, function (todo) {
+        !todo.done && $scope.todos.push(todo);
+      });
+      $window.localStorage.setItem('itemsList', angular.toJson($scope.todos))
+
+    } else {
+      console.log("No items listed yet...")
+    }
+
   };
 
-  $scope.deleteAll = function() {
+  $scope.deleteAll = function () {
     $scope.todos.length = 0;
     $window.localStorage.removeItem('itemsList')
   }
 
-  $scope.deleteItem = function() {
+  $scope.deleteItem = function () {
     $scope.todos.splice(this.$index, 1);
     $window.localStorage.setItem('itemsList', angular.toJson($scope.todos))
   }
-  
-  $scope.updateItem = function($event) {
-    console.log($event)
+
+  $scope.updateItem = function () {
+    console.log()
     // $window.localStorage.setItem('itemsList', angular.toJson($scope.todos))
 
   }
 
-  $scope.toggleEdit = function($event) {
-    $event.target.previousElementSibling.focus()
+  $scope.toggleEdit = function () {
+    console.log()
+    // $event.target.previousElementSibling.focus()
+  }
+
+
+  $scope.saveEdit = function () {
+    console.log()
+    // this.todo.text = $event.target.previousElementSibling.value;
+    // $window.localStorage.setItem('itemsList', angular.toJson($scope.todos))
   }
 });
